59cbb9a4cf661015ffab0977074b73d5c40bf1b4
David Cornejo <koerdev@gmail.com>
2026-01-30 18:42:25 -0300

    refactor: se cambia el uso de selectores a jquery en el login

--- js/login.js
+++ js/login.js
@@ -1,21 +1,26 @@
-let emailInput = document.getElementById("email")
-let passInput = document.getElementById("password")
+// se cambian los selectores de los id para email y password para seleccionarlos con jquery
+// let emailInput = document.getElementById("email")
+// let passInput = document.getElementById("password")
 let btnLogin = document.getElementById("btnLogin")
 let errorMessage = document.getElementById("errorMessage")
 
 const validEmail = "admin@email.com"
 const validPass = "12345"
 
-btnLogin.addEventListener("click", function (e) {
+// se cambia el event listener para usar jquery
+// btnLogin.addEventListener("click", function (e) {
+$('#loginForm').submit(function(e) {
     e.preventDefault()
-    let email = emailInput.value.trim()
-    let pass = passInput.value.trim()
+    // ahora se obtienen los valores directamente cuando se hace click en el botón
+    let email = $('#email').val().trim()
+    let pass = $('#password').val().trim()
 
     if(email === validEmail && pass === validPass) {
         alert("Credenciales validas")
         window.location.href = "./pages/menu.html"
     } else {
         errorMessage.textContent = "Email o contraseña incorrectos"
+        $('#errorMessage').removeClass('d-none')
         return
     }
 })
